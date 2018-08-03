276b43b0e66019d3c8b64d34fc7b7910dcb481c4
Poren Chiang <ren.chiang@gmail.com>
2018-08-03 22:07:05 +0800

    Add convenience functions

--- profile.cpp
+++ profile.cpp
@@ -1,6 +1,7 @@
 #include "profile.h"
 #include <QJsonDocument>
 #include <QDebug>
+#include <QDateTime>
 
 using namespace Plurq;
 
@@ -8,7 +9,41 @@ Profile::Profile(QIODevice* stream)
     : Entity::Entity(stream)
 {}
 
-QJsonValue Profile::displayName() const
+QUrl Profile::avatarUrl(Avatar size) const
 {
-    return m_entity.value("display_name");
+    bool hasProfileImage = (m_entity[QLatin1String("has_profile_image")].toInt() == 1);
+    auto avatar = m_entity[QLatin1String("avatar")];
+
+    if (!hasProfileImage) {
+        switch (size) {
+        case Avatar::Small:
+            return QUrl(QStringLiteral("https://www.plurk.com/static/default_small.gif"));
+        case Avatar::Medium:
+            return QUrl(QStringLiteral("https://www.plurk.com/static/default_medium.gif"));
+        case Avatar::Large:
+            return QUrl(QStringLiteral("https://www.plurk.com/static/default_big.gif"));
+        }
+    } else {
+        QString version = avatar.isNull() ? "" : QString::number(avatar.toInt());
+        switch (size) {
+        case Avatar::Small:
+            return QUrl(QStringLiteral("https://avatars.plurk.com/%1-small%2.gif").arg(id()).arg(version));
+        case Avatar::Medium:
+            return QUrl(QStringLiteral("https://avatars.plurk.com/%1-medium%2.gif").arg(id()).arg(version));
+        case Avatar::Large:
+            return QUrl(QStringLiteral("https://avatars.plurk.com/%1-large%2.jpg").arg(id()).arg(version));
+        }
+    }
+}
+
+bool Profile::birthday() const
+{
+    if (m_entity[QLatin1String("bday_privacy")].toInt() != 0) {
+        QString value = m_entity[QLatin1String("date_of_birth")].toString();
+        QDate date = QDateTime::fromString(value, Qt::RFC2822Date).date();
+        QDate today = QDate::currentDate();
+        return (date.month() == today.month()) && (date.day() == today.day());
+    } else {
+        return false;
+    }
 }
--- profile.h
+++ profile.h
@@ -1,16 +1,33 @@
 #ifndef PROFILE_H
 #define PROFILE_H
 
+#include <QUrl>
 #include "entity.h"
 
 namespace Plurq {
+
+enum Avatar
+{
+    Small,
+    Medium,
+    Large
+};
 
 class Profile : public Entity
 {
 public:
     Profile(QIODevice* stream);
 
-    QJsonValue displayName() const;
+    inline int id() const { return m_entity[QLatin1String("id")].toInt(); }
+    inline QString nickName() const { return m_entity[QLatin1String("nick_name")].toString(); }
+    inline QString displayName() const { return m_entity[QLatin1String("display_name")].toString(); }
+    inline QString location() const { return m_entity[QLatin1String("location")].toString(); }
+    inline QString fullName() const { return m_entity[QLatin1String("full_name")].toString(); }
+    inline double karma() const { return m_entity[QLatin1String("karma")].toDouble(); }
+    inline bool premium() const { return m_entity[QLatin1String("premium")].toBool(); }
+
+    QUrl avatarUrl(Avatar size = Avatar::Medium) const;
+    bool birthday() const;
 };
 
 }
