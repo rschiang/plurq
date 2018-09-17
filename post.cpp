8cbd0bd79aa943ed54ffd43ee85db24709df2848
Poren Chiang <ren.chiang@gmail.com>
2018-09-17 19:32:10 +0800

    Add convenience methods on post

--- post.cpp
+++ post.cpp
@@ -6,3 +6,9 @@ bool Post::secret() const
 {
     return !((*this)[QLatin1String("limited_to")].isNull());
 }
+
+QString Post::translatedQualifier() const
+{
+    // Return translated qualifier if applicable
+    return (*this)[QLatin1String("qualifier_translated")].toString(qualifier());
+}
--- post.h
+++ post.h
@@ -25,9 +25,9 @@ class Post : public Entity
 public:
     // Plurk data
     inline int id() const { return intValue(QLatin1String("plurk_id")); }
+    inline int userId() const { return intValue(QLatin1String("user_id")); }
     inline int ownerId() const { return intValue(QLatin1String("owner_id")); }
     inline QString qualifier() const { return stringValue(QLatin1String("qualifier")); }
-    inline QString translatedQualifier() const { return stringValue(QLatin1String("qualifier_translated")); }
     inline QString content() const { return stringValue(QLatin1String("content")); }
     inline QString rawContent() const { return stringValue(QLatin1String("content_raw")); }
     inline QString language() const { return stringValue(QLatin1String("lang")); }
@@ -56,6 +56,7 @@ public:
     inline bool bookmarked() const { return boolValue(QLatin1String("bookmark")); }
 
     bool secret() const;
+    QString translatedQualifier() const;
 };
 
 }
